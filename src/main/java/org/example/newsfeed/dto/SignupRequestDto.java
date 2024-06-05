8a1de8950d8716ae7d9c513ffd693ec121a862cb
bbackbbacke <157469179+bbackbbacke@users.noreply.github.com>
2024-06-05 14:26:08 +0900

    @NotBlank, @Email

--- src/main/java/org/example/newsfeed/dto/SignupRequestDto.java
+++ src/main/java/org/example/newsfeed/dto/SignupRequestDto.java
@@ -1,12 +1,18 @@
 package org.example.newsfeed.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class SignupRequestDto {
+    @NotBlank
     private String user_id;
+    @NotBlank
     private String password;
+    @Email
+    @NotBlank
     private String email;
 }
