4924989d28fa45b1e4583411df7365039ffaa046
bbackbbacke <157469179+bbackbbacke@users.noreply.github.com>
2024-06-05 12:57:32 +0900

    (서비스)회원 중복, 이메일 중복, 저장

--- src/main/java/org/example/newsfeed/service/UserService.java
+++ src/main/java/org/example/newsfeed/service/UserService.java
@@ -2,16 +2,46 @@ package org.example.newsfeed.service;
 
 import lombok.RequiredArgsConstructor;
 import org.example.newsfeed.dto.SignupRequestDto;
+import org.example.newsfeed.entity.User;
 import org.example.newsfeed.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-@RequiredArgsConstructor
+
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public static void signup(SignupRequestDto requestDto) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void signup(SignupRequestDto requestDto) {
+        String user_id = requestDto.getUser_id();
+        String password = passwordEncoder.encode(requestDto.getPassword());
+
+        // 회원 중복 확인
+        Optional<User> checkUser_id = userRepository.findByUserId(user_id);
+        if (checkUser_id.isPresent()) {
+            throw new IllegalArgumentException("중복된 사용자가 존재합니다.");
+        }
+
+        // email 중복확인
+        String email = requestDto.getEmail();
+        Optional<User> checkEmail = userRepository.findByEmail(email);
+        if (checkEmail.isPresent()) {
+            throw new IllegalArgumentException("중복된 Email 입니다.");
+        }
+        // 사용자 등록
+        User user = new User(user_id, password, email);
+        userRepository.save(user);
 
     }
+
+
 }
