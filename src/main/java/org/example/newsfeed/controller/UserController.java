65a61dcde18b4e15fc0433b53eec705acfce0ef9
bbackbbacke <157469179+bbackbbacke@users.noreply.github.com>
2024-06-05 14:24:11 +0900

    validation추가, controller 유효성 검사

--- src/main/java/org/example/newsfeed/controller/UserController.java
+++ src/main/java/org/example/newsfeed/controller/UserController.java
@@ -1,21 +1,26 @@
 package org.example.newsfeed.controller;
 
+import jakarta.validation.Valid;
+import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.newsfeed.dto.SignupRequestDto;
 import org.example.newsfeed.entity.User;
 import org.example.newsfeed.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @AllArgsConstructor
 @RequestMapping("/api")
 public class UserController {
 
     private final UserService userService;
-
 
 
     //회원가입 페이지 -> html파일 강의에서 쓰던거 쓸 것인가 추후 정하기
@@ -25,12 +30,19 @@ public class UserController {
     }
 
     @PostMapping("/signup")
-    public String signup(SignupRequestDto requestDto) {
+    public String signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+        // Validation 예외처리 (유효성)
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if (fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+            }
+            return "redirect:/api/signup";
+        }
         userService.signup(requestDto);
-        return "redirect:/api/login";
+
+        return "redirect :/api/login";
+
     }
-
-
-
 
 }
