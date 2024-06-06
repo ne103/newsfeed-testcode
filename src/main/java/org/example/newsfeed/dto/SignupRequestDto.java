4cf6e4dd9efc46e9d3bcbb59ead9a350f4c5a3b1
bbackbbacke <157469179+bbackbbacke@users.noreply.github.com>
2024-06-06 16:30:50 +0900

    회원가입시 name, email, comment requestdto 추가

--- src/main/java/org/example/newsfeed/dto/SignupRequestDto.java
+++ src/main/java/org/example/newsfeed/dto/SignupRequestDto.java
@@ -1,6 +1,5 @@
 package org.example.newsfeed.dto;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,8 +7,12 @@ import lombok.Setter;
 @Getter
 @Setter
 public class SignupRequestDto {
+    private String name;
     @NotBlank
     private String userId;
     @NotBlank
     private String password;
+    private String email;
+    private String comment;
+
 }
