22836bda8369d70e64c94d8f550f8e77183581f6
byeongik11 <mice2008@naver.com>
2024-06-09 18:34:30 +0900

    feat 쿼리 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO newsfeed (user_id,password,name,email,comment,status,status_change_time,create_date,modify_date)
+INSERT INTO user (user_id,password,name,email,comment,status,status_change_time,create_date,modify_date)
 VALUES ('abc1234','1234','이병익','mice2008@naver.com','내용테스트','정상',current_timestamp ,current_timestamp,CURRENT_TIMESTAMP);
-INSERT INTO newsfeed (id, name) VALUES ('Team Standup', '2024-06-02T09:00:00');
+INSERT INTO user (id, name) VALUES ('Team Standup', '2024-06-02T09:00:00');
 
-DELETE FROM newsfeed WHERE user_id = 'abc1234';
+DELETE FROM user WHERE user_id = 'abc1234';
