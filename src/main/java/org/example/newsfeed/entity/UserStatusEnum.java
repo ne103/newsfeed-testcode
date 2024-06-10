b1b642afe448d9e84bd6ea64cfd95c73aee7c34a
bbackbbacke <157469179+bbackbbacke@users.noreply.github.com>
2024-06-10 13:10:51 +0900

    UserStatusEnum정리

--- src/main/java/org/example/newsfeed/entity/UserStatusEnum.java
+++ src/main/java/org/example/newsfeed/entity/UserStatusEnum.java
@@ -1,6 +1,16 @@
 package org.example.newsfeed.entity;
 
+import lombok.Getter;
+
+@Getter
 public enum UserStatusEnum {
-    ACTIVE,
-    WITHDRAWN
-}
+    ACTIVE("정상 회원입니다."),
+    WITHDRAWN("탈퇴한 회원입니다.");
+
+    private final String status;
+
+    UserStatusEnum(String status) {
+        this.status = status;
+
+    }
+    }
