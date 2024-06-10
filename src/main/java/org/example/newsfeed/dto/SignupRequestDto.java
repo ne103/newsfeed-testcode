d10b99da938a05002540811c1690643e3be65e23
bbackbbacke <157469179+bbackbbacke@users.noreply.github.com>
2024-06-10 13:09:36 +0900

    유효성 검사 SignupRequestDto로 옮김, UserService 로직 정리

--- src/main/java/org/example/newsfeed/dto/SignupRequestDto.java
+++ src/main/java/org/example/newsfeed/dto/SignupRequestDto.java
@@ -1,19 +1,26 @@
 package org.example.newsfeed.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
+import org.example.newsfeed.entity.UserStatusEnum;
 
 @Getter
 @Setter
 public class SignupRequestDto {
     private String name;
-    @NotBlank
+
+    @Pattern(regexp = "^[A-Za-z\\d]{10,20}$")
     private String userId;
-    @NotBlank
+
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*?.,;:])[A-Za-z\\d!@#$%^&*?.,;:]{10,}$")
     private String password;
+
+    @Email
     private String email;
     private String comment;
-    private boolean withdraw;
+
 
 }
--- src/main/java/org/example/newsfeed/service/UserService.java
+++ src/main/java/org/example/newsfeed/service/UserService.java
@@ -1,131 +1,101 @@
 package org.example.newsfeed.service;
 
-import java.util.regex.Pattern;
+import java.util.Objects;
+import java.util.Optional;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.newsfeed.dto.PasswordRequestDto;
 import org.example.newsfeed.dto.SignupRequestDto;
-import org.example.newsfeed.dto.WithdrawRequestDto;
 import org.example.newsfeed.entity.User;
-import org.example.newsfeed.entity.UserRoleEnum;
 import org.example.newsfeed.entity.UserStatusEnum;
 import org.example.newsfeed.exception.AlreadyWithdrawnUserException;
-import org.example.newsfeed.exception.InvalidPasswordException;
 import org.example.newsfeed.exception.PasswordMismatchException;
 import org.example.newsfeed.exception.UserIdNotFoundException;
 import org.example.newsfeed.repository.UserRepository;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+    private static final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
-
-
-    // 사용자 ID 유효성 검사
-    private void validateUserId(String userId) {
-        if (userId.length() < 10 || userId.length() > 20) {
-            throw new IllegalArgumentException("사용자 ID는 최소 10글자 이상, 최대 20글자 이하여야 합니다.");
-        }
-        if (!userId.matches("[a-zA-Z0-9]+")) {
-            throw new IllegalArgumentException("사용자 ID는 대소문자 포함 영문, 숫자만을 허용합니다.");
-        }
-    }
-
-    // 비밀번호 유효성 검사
-    private void validatePassword(String password) {
-        if (password.length() < 10) {
-            throw new IllegalArgumentException("비밀번호는 최소 10글자 이상이어야 합니다.");
-        }
-
-        if (Pattern.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{10,}$", password)) {
-            throw new IllegalArgumentException("비밀번호는 대소문자 포함 영문, 숫자, 특수문자를 최소 1글자씩 포함해야 합니다.");
-        }
-
-    }
-
+    // 비밀번호 암호화
     private String encodePassword(String password) {
         return passwordEncoder.encode(password);
     }
 
-    // ID 일치 여부 (유효성 검사)
-    public void withdraw(WithdrawRequestDto requestDto) {
-        User user = userRepository.findByUserId(requestDto.getUserId())
-            .orElseThrow(() -> new UserIdNotFoundException("해당 사용자를 찾을 수 없습니다."));
-
-        // 비밀번호 일치 여부
-        if (!passwordEncoder.matches(requestDto.getPassword(), user.getPassword())) {
-            throw new InvalidPasswordException("비밀번호가 일치하지 않습니다.");
-        }
-
-        //회원 탈퇴 및 상태 변경
-        user.setStatus(UserStatusEnum.WITHDRAWN.name());
-        userRepository.save(user);
-    }
-
+    // 회원가입
     public void signup(SignupRequestDto requestDto) {
         String userId = requestDto.getUserId();
-        String password = passwordEncoder.encode(requestDto.getPassword());
+        String password = encodePassword(requestDto.getPassword());
 
-        // 사용자 ID 유효성 검사
-        validateUserId(userId);
+        User user = new User(
+            userId,
+            password,
+            requestDto.getName(),
+            requestDto.getEmail(),
+            requestDto.getComment(),
+            "",
+            "",
+            UserStatusEnum.ACTIVE
+        );
 
-        // 비밀번호 유효성 검사
-        validatePassword(password);
+        registerUser(user);
+    }
 
-        // 중복 ID 체크
-        Optional<User> checkUser_id = userRepository.findByUserIdAndStatus(requestDto.getUserId(),
-            UserStatusEnum.ACTIVE.name());
-        if (checkUser_id.isPresent()) {
-            throw new IllegalArgumentException("중복된 사용자가 존재합니다.");
+    // DB 값 중복 입력 시 오류 처리
+    private void registerUser(User user) {
+        try {
+            userRepository.save(user);
+        } catch (DataIntegrityViolationException ex) {
+            String errorMessage = Objects.requireNonNull(ex.getRootCause()).getMessage();
+            if (errorMessage.contains("Duplicate entry") && errorMessage.contains("user_id")) {
+                throw new IllegalArgumentException("ID already exists!");
+            }
+            throw new IllegalArgumentException("An unknown error occurred");
         }
-
-        // 회원가입 (회원 상태 ACTIVE로 설정)
-        User user = new User(requestDto.getUserId(), requestDto.getPassword(),
-            UserStatusEnum.ACTIVE.name(),
-            UserRoleEnum.USER);
-        userRepository.save(user);
     }
 
-    //Active상태만 조회
-    Optional<User> findByUserIdAndStatus(String userId, String status) {
-        return userRepository.findByUserIdAndStatus(userId, status);
-    }
-
-
-    private boolean isExistingOrWithdrawnId(String userId) {
-        Optional<User> existingUser = userRepository.findByUserId(userId);
-        if (existingUser.isPresent()) {
-            return true; // 중복된 ID
-        }
-
-         // *************************** 탈퇴한 ID => Status가 Active인 걸 찾으면 되잖어~
-
-        return false; // 사용가능한 ID
-    }
-
-
+    // 회원 탈퇴 처리
+    @Transactional
     public void withdrawUser(String userId, String password) {
         User user = userRepository.findByUserId(userId)
             .orElseThrow(() -> new UserIdNotFoundException("존재하지 않는 이용자입니다."));
 
-        if (user.isWithdraw()) {
+        if (UserStatusEnum.WITHDRAWN.equals(user.getStatus())) {
             throw new AlreadyWithdrawnUserException("이미 탈퇴된 사용자입니다.");
         }
 
-        if (!user.getPassword().equals(password)) {
+        if (!passwordEncoder.matches(password, user.getPassword())) {
             throw new PasswordMismatchException("비밀번호가 일치하지 않습니다.");
         }
 
-        user.withdraw();
+        user.setStatus(UserStatusEnum.WITHDRAWN);
         userRepository.save(user);
     }
 
-}
+    // 회원 탈퇴 (소프트 삭제)
+    @Transactional
+    public void deleteAccount(String userId, PasswordRequestDto passwordRequestDto) {
+        log.info("삭제된 계정");
+        User user = userRepository.findByUserId(userId)
+            .orElseThrow(() -> new UserIdNotFoundException("존재하지 않는 이용자입니다."));
 
+        if (!passwordEncoder.matches(passwordRequestDto.getPassword(), user.getPassword())) {
+            log.info("회원탈퇴 취소");
+            throw new PasswordMismatchException("비밀번호가 일치하지 않습니다.");
+        }
+
+        user.setStatus(UserStatusEnum.WITHDRAWN);
+        userRepository.save(user);
+    }
+
+
+}
