7cf0170349d1bc5f2603106311e25280fa18866c
bbackbbacke <157469179+bbackbbacke@users.noreply.github.com>
2024-06-10 13:09:02 +0900

    newsfeed엔티티 정리

--- src/main/java/org/example/newsfeed/entity/Newsfeed.java
+++ src/main/java/org/example/newsfeed/entity/Newsfeed.java
@@ -2,6 +2,7 @@ package org.example.newsfeed.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,13 +12,17 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.sql.Timestamp;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "newsfeed")
 public class Newsfeed {
     @Id
