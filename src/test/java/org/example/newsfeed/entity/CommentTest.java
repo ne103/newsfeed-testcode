62bdd9d28667413097119a95ba7ad408cc517cc9
ne103 <ne97@naver.com>
2024-06-17 06:57:43 +0900

    refactor : 엔티티 테스트 코드 리펙토링

--- src/test/java/org/example/newsfeed/entity/CommentTest.java
+++ src/test/java/org/example/newsfeed/entity/CommentTest.java
@@ -5,10 +5,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-public class CommentTest {
+@ExtendWith(MockitoExtension.class)
+class CommentTest {
 
     @Mock
     private User user;
